b58c38a1a1ac671912b2d80b440cf0d5ae653581
rvpacheco <rvpacheco17@gmail.com>
2023-03-18 01:39:03 +0000

    Mi primer commit

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,37 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
-  // reset the website body with the new html output
+  const {
+    name,
+    lastname,
+    role,
+    city,
+    country,
+    twitter,
+    github,
+    instagram,
+    linkedin
+  } = variables; // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
+            
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name ? name : " name"} ${lastname ? lastname : " last"}</h1>
+          <h2>${role ? role : " role"}</h2>
+          <h3>${city ? city : " city"}, ${country ? country : " country"}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${
+              twitter ? twitter : " "
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              github ? github : " "
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              linkedin ? linkedin : " "
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              instagram ? instagram : " "
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
